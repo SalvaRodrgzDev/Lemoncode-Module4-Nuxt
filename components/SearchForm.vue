dc0e7c4a9ea8a8714e2883cf5afe864059366b99
salva <salva94rodri@gmail.com>
2021-03-11 19:51:23 +0100

    divide components

---
+++ components/SearchForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-form @submit.prevent="updateSearchTerm">
+            <v-text-field
+              :append-icon="'mdi-magnify'"
+              label="Search..."
+              v-model="searchterm"
+            >
+            </v-text-field>
+          </v-form>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      searchterm: "lemoncode",
+    }
+  },
+  methods: {
+    async updateSearchTerm() {
+      this.$store.commit('setSearchTerm', this.searchterm)
+    },
+  }
+})
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -8,14 +8,7 @@
     >
         <v-toolbar-title>Github Members</v-toolbar-title>
         <v-spacer></v-spacer>
-          <v-form @submit.prevent="updateSearchTerm">
-            <v-text-field
-              :append-icon="'mdi-magnify'"
-              label="Search..."
-              v-model="searchterm"
-            >
-            </v-text-field>
-          </v-form>
+        <SearchForm />
     </v-app-bar>
     <v-main
       id="scrolling-techniques-7"
@@ -26,19 +19,3 @@
   </v-card>
   </v-app>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-export default Vue.extend({
-  data() {
-    return {
-      searchterm: "lemoncode",
-    }
-  },
-  methods: {
-    async updateSearchTerm() {
-      this.$store.commit('setSearchTerm', this.searchterm)
-    },
-  }
-})
-</script>
